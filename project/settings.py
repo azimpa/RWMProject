91189b9419c2e931aee3a1256854eba6786427c7
azimpa <muhammedasim.pa@gmail.com>
2023-10-10 12:19:14 +0530

    git cmnt 3

--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_DIRS = [BASE_DIR / "static",]
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
